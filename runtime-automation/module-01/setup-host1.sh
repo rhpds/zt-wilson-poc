696f7f43ee9d5ef2b9073f560303b7201c7b82e7
Wilson Harris <wharris@redhat.com>
2026-04-06 10:05:45 -0400

    Update for litemas and usb testing

--- runtime-automation/module-01/setup-host1.sh
+++ runtime-automation/module-01/setup-host1.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
 echo "Starting module called module-01" >> /tmp/progress.log
+
+cat > /home/rhel/runtime-env-check.txt << EOF
+GUID=${GUID}
+DOMAIN=${DOMAIN}
+LITELLM_API_URL=${LITELLM_API_URL}
+LITELLM_API_KEY=${LITELLM_API_KEY}
+EOF
+chown rhel:rhel /home/rhel/runtime-env-check.txt
--- setup-automation/setup-host1.sh
+++ setup-automation/setup-host1.sh
@@ -4,9 +4,14 @@ USER=rhel
 echo "Adding wheel" > /root/post-run.log
 usermod -aG wheel rhel
 
-echo "Setup vm control01" > /tmp/progress.log
+echo "Setup vm host1" > /tmp/progress.log
+chmod 666 /tmp/progress.log
 
-chmod 666 /tmp/progress.log 
-
-#dnf install -y nc
+cat > /home/rhel/setup-env-check.txt << EOF
+GUID=${GUID}
+DOMAIN=${DOMAIN}
+LITELLM_API_URL=${LITELLM_API_URL}
+LITELLM_API_KEY=${LITELLM_API_KEY}
+EOF
+chown rhel:rhel /home/rhel/setup-env-check.txt
 
